5e620314c093d1599724734c848d5edd71171c93
Stefan Guilhen <sguilhen@redhat.com>
2010-02-02 18:40:59 +0000

    issueToken and renewToken methods now first try to locate the RequestedSecurityToken using both element name and namespace. If that fails (responses that are not properly schema driven), use only the element name
    git-svn-id: https://svn.jboss.org/repos/picketlink/federation/trunk@123 cbddccc4-e45a-42eb-be77-0d76e4b45d38

--- picketlink-fed-core/src/main/java/org/picketlink/identity/federation/core/wstrust/STSClient.java
+++ picketlink-fed-core/src/main/java/org/picketlink/identity/federation/core/wstrust/STSClient.java
@@ -59,217 +59,232 @@ import org.w3c.dom.NodeList;
  */
 public class STSClient
 {
-    private ThreadLocal<Dispatch<Source>> dispatchLocal = new InheritableThreadLocal<Dispatch<Source>>();
+   private ThreadLocal<Dispatch<Source>> dispatchLocal = new InheritableThreadLocal<Dispatch<Source>>();
 
-    private String targetNS = "http://org.picketlink.trust/sts/";
+   private String targetNS = "http://org.picketlink.trust/sts/";
 
-    public STSClient(STSClientConfig config)
-    {
-        QName service = new QName(targetNS, config.getServiceName());
-        QName portName = new QName(targetNS, config.getPortName());
+   public STSClient(STSClientConfig config)
+   {
+      QName service = new QName(targetNS, config.getServiceName());
+      QName portName = new QName(targetNS, config.getPortName());
 
-        Service jaxwsService = Service.create(service);
-        jaxwsService.addPort(portName, SOAPBinding.SOAP11HTTP_BINDING, config.getEndPointAddress());
-        Dispatch<Source> dispatch = jaxwsService.createDispatch(portName, Source.class, Mode.PAYLOAD);
+      Service jaxwsService = Service.create(service);
+      jaxwsService.addPort(portName, SOAPBinding.SOAP11HTTP_BINDING, config.getEndPointAddress());
+      Dispatch<Source> dispatch = jaxwsService.createDispatch(portName, Source.class, Mode.PAYLOAD);
 
-        Map<String, Object> reqContext = dispatch.getRequestContext();
-        String username = config.getUsername();
-        if (username != null)
-        {
-	        // add the username and password to the request context.
-            reqContext.put(BindingProvider.USERNAME_PROPERTY, config.getUsername());
-            reqContext.put(BindingProvider.PASSWORD_PROPERTY, config.getPassword());
-        }
-        dispatchLocal.set(dispatch);
-    }
-    
-    /**
-     * Issues a Security Token for the ultimate recipient of the token.
-     * 
-     * @param endpointURI - The ultimate recipient of the token. This will be set at the AppliesTo for
-     *                      the RequestSecurityToken which is an optional element so it may be null.
-     * @return Element - The Security Token Element which will be of the TokenType configured
-     *                  for the endpointURI passed in.
-     * @throws WSTrustException
-     */
-    public Element issueTokenForEndpoint(String endpointURI) throws WSTrustException
-    {
-        RequestSecurityToken request = new RequestSecurityToken();
-        setAppliesTo(endpointURI, request);
-        return issueToken(request);
-    }
-    
-    /**
-     * Issues a Security Token from the STS. This methods has the option of 
-     * specifying one or both of endpointURI/tokenType but at least one must 
-     * specified.
-     * 
-     * @param endpointURI - The ultimate recipient of the token. This will be set at the AppliesTo for
-     *                      the RequestSecurityToken which is an optional element so it may be null.
-     * @param tokenType - The type of security token to be issued.
-     * @return Element - The Security Token Element issued.
-     * @throws IllegalArgumentException If neither endpointURI nor tokenType was specified.
-     * @throws WSTrustException
-     */
-    public Element issueToken(String endpointURI, String tokenType) throws WSTrustException
-    {
-        if (endpointURI == null && tokenType == null)
-            throw new IllegalArgumentException("One of endpointURI or tokenType must be provided.");
-        
-        RequestSecurityToken request = new RequestSecurityToken();
-        setAppliesTo(endpointURI, request);
-        setTokenType(tokenType, request);
-        return issueToken(request);
-    }
-    
-    public Element issueToken(String tokenType) throws WSTrustException
-    {
-        // create a custom token request message.
-        RequestSecurityToken request = new RequestSecurityToken();
-        setTokenType(tokenType, request);
-        // send the token request to JBoss STS and get the response.
-        return issueToken(request);
-    }
+      Map<String, Object> reqContext = dispatch.getRequestContext();
+      String username = config.getUsername();
+      if (username != null)
+      {
+         // add the username and password to the request context.
+         reqContext.put(BindingProvider.USERNAME_PROPERTY, config.getUsername());
+         reqContext.put(BindingProvider.PASSWORD_PROPERTY, config.getPassword());
+      }
+      dispatchLocal.set(dispatch);
+   }
 
-    private RequestSecurityToken setAppliesTo(String endpointURI, RequestSecurityToken rst)
-    {
-        if (endpointURI != null)
-            rst.setAppliesTo(WSTrustUtil.createAppliesTo(endpointURI));
-        return rst;
-    }
+   /**
+    * Issues a Security Token for the ultimate recipient of the token.
+    * 
+    * @param endpointURI - The ultimate recipient of the token. This will be set at the AppliesTo for
+    *                      the RequestSecurityToken which is an optional element so it may be null.
+    * @return Element - The Security Token Element which will be of the TokenType configured
+    *                  for the endpointURI passed in.
+    * @throws WSTrustException
+    */
+   public Element issueTokenForEndpoint(String endpointURI) throws WSTrustException
+   {
+      RequestSecurityToken request = new RequestSecurityToken();
+      setAppliesTo(endpointURI, request);
+      return issueToken(request);
+   }
 
-    private RequestSecurityToken setTokenType(String tokenType, RequestSecurityToken rst)
-    {
-        if (tokenType != null)
-            rst.setTokenType(URI.create(tokenType));
-        return rst;
-    }
+   /**
+    * Issues a Security Token from the STS. This methods has the option of 
+    * specifying one or both of endpointURI/tokenType but at least one must 
+    * specified.
+    * 
+    * @param endpointURI - The ultimate recipient of the token. This will be set at the AppliesTo for
+    *                      the RequestSecurityToken which is an optional element so it may be null.
+    * @param tokenType - The type of security token to be issued.
+    * @return Element - The Security Token Element issued.
+    * @throws IllegalArgumentException If neither endpointURI nor tokenType was specified.
+    * @throws WSTrustException
+    */
+   public Element issueToken(String endpointURI, String tokenType) throws WSTrustException
+   {
+      if (endpointURI == null && tokenType == null)
+         throw new IllegalArgumentException("One of endpointURI or tokenType must be provided.");
 
-    private Element issueToken(RequestSecurityToken request) throws WSTrustException
-    {
-        request.setRequestType(URI.create(WSTrustConstants.ISSUE_REQUEST));
-        request.setContext("context");
-        WSTrustJAXBFactory jaxbFactory = WSTrustJAXBFactory.getInstance();
-        DOMSource requestSource = (DOMSource) jaxbFactory.marshallRequestSecurityToken(request);
-        Source response = dispatchLocal.get().invoke(requestSource);
+      RequestSecurityToken request = new RequestSecurityToken();
+      setAppliesTo(endpointURI, request);
+      setTokenType(tokenType, request);
+      return issueToken(request);
+   }
 
-        Node documentNode = ((DOMSource) response).getNode();
-        Document responseDoc = documentNode instanceof Document ? (Document) documentNode : documentNode.getOwnerDocument();
+   public Element issueToken(String tokenType) throws WSTrustException
+   {
+      // create a custom token request message.
+      RequestSecurityToken request = new RequestSecurityToken();
+      setTokenType(tokenType, request);
+      // send the token request to JBoss STS and get the response.
+      return issueToken(request);
+   }
 
-        NodeList nodes;
-        try
-        {
-            Document myDocument = DocumentUtil.createDocument();
-            Node importedNode = myDocument.importNode(responseDoc.getDocumentElement(), true);
-            myDocument.appendChild(importedNode);
+   private RequestSecurityToken setAppliesTo(String endpointURI, RequestSecurityToken rst)
+   {
+      if (endpointURI != null)
+         rst.setAppliesTo(WSTrustUtil.createAppliesTo(endpointURI));
+      return rst;
+   }
 
-            nodes = null;
-            if (responseDoc instanceof SOAPPart)
-            {
-                SOAPPart soapPart = (SOAPPart) responseDoc;
-                SOAPEnvelope env = soapPart.getEnvelope();
-                SOAPBody body = env.getBody();
-                Node data = body.getFirstChild();
-                nodes = ((Element) data).getElementsByTagName("RequestedSecurityToken");
-            }
-            else
-                nodes = responseDoc.getElementsByTagNameNS(WSTrustConstants.BASE_NAMESPACE, "RequestedSecurityToken");
-        }
-        catch (Exception e)
-        {
-            throw new WSTrustException("Exception in issuing token:", e);
-        }
+   private RequestSecurityToken setTokenType(String tokenType, RequestSecurityToken rst)
+   {
+      if (tokenType != null)
+         rst.setTokenType(URI.create(tokenType));
+      return rst;
+   }
 
-        if (nodes == null)
-            throw new WSTrustException("NodeList is null");
+   private Element issueToken(RequestSecurityToken request) throws WSTrustException
+   {
+      request.setRequestType(URI.create(WSTrustConstants.ISSUE_REQUEST));
+      request.setContext("context");
+      WSTrustJAXBFactory jaxbFactory = WSTrustJAXBFactory.getInstance();
+      DOMSource requestSource = (DOMSource) jaxbFactory.marshallRequestSecurityToken(request);
+      Source response = dispatchLocal.get().invoke(requestSource);
 
-        Node rstr = nodes.item(0);
+      Node documentNode = ((DOMSource) response).getNode();
+      Document responseDoc = documentNode instanceof Document ? (Document) documentNode : documentNode
+            .getOwnerDocument();
 
-        return (Element) rstr.getFirstChild();
-    }
+      NodeList nodes;
+      try
+      {
+         Document myDocument = DocumentUtil.createDocument();
+         Node importedNode = myDocument.importNode(responseDoc.getDocumentElement(), true);
+         myDocument.appendChild(importedNode);
 
-    public Element renewToken(String tokenType, Element token) throws WSTrustException
-    {
-        RequestSecurityToken request = new RequestSecurityToken();
-        request.setContext("context");
+         nodes = null;
+         if (responseDoc instanceof SOAPPart)
+         {
+            SOAPPart soapPart = (SOAPPart) responseDoc;
+            SOAPEnvelope env = soapPart.getEnvelope();
+            SOAPBody body = env.getBody();
+            Node data = body.getFirstChild();
+            nodes = ((Element) data).getElementsByTagNameNS(WSTrustConstants.BASE_NAMESPACE, "RequestedSecurityToken");
+            if (nodes == null || nodes.getLength() == 0)
+               nodes = ((Element) data).getElementsByTagName("RequestedSecurityToken");
+         }
+         else
+         {
+            nodes = responseDoc.getElementsByTagNameNS(WSTrustConstants.BASE_NAMESPACE, "RequestedSecurityToken");
+            if (nodes == null || nodes.getLength() == 0)
+               nodes = responseDoc.getElementsByTagName("RequestedSecurityToken");
+         }
+      }
+      catch (Exception e)
+      {
+         throw new WSTrustException("Exception in issuing token:", e);
+      }
 
-        request.setTokenType(URI.create(WSTrustConstants.STATUS_TYPE));
-        request.setRequestType(URI.create(WSTrustConstants.RENEW_REQUEST));
-        RenewTargetType renewTarget = new RenewTargetType();
-        renewTarget.setAny(token);
-        request.setRenewTarget(renewTarget);
+      if (nodes == null)
+         throw new WSTrustException("NodeList is null");
 
-        // send the token request to JBoss STS and get the response.
-        WSTrustJAXBFactory jaxbFactory = WSTrustJAXBFactory.getInstance();
-        DOMSource requestSource = (DOMSource) jaxbFactory.marshallRequestSecurityToken(request);
-        Source response = dispatchLocal.get().invoke(requestSource);
+      Node rstr = nodes.item(0);
 
-        Node documentNode = ((DOMSource) response).getNode();
-        Document responseDoc = documentNode instanceof Document ? (Document) documentNode : documentNode.getOwnerDocument();
+      return (Element) rstr.getFirstChild();
+   }
 
-        NodeList nodes;
-        try
-        {
-            Document myDocument = DocumentUtil.createDocument();
-            Node importedNode = myDocument.importNode(responseDoc.getDocumentElement(), true);
-            myDocument.appendChild(importedNode);
+   public Element renewToken(String tokenType, Element token) throws WSTrustException
+   {
+      RequestSecurityToken request = new RequestSecurityToken();
+      request.setContext("context");
 
-            nodes = null;
-            if (responseDoc instanceof SOAPPart)
-            {
-                SOAPPart soapPart = (SOAPPart) responseDoc;
-                SOAPEnvelope env = soapPart.getEnvelope();
-                SOAPBody body = env.getBody();
-                Node data = body.getFirstChild();
-                nodes = ((Element) data).getElementsByTagName("RequestedSecurityToken");
-            }
-            else
-                nodes = responseDoc.getElementsByTagNameNS(WSTrustConstants.BASE_NAMESPACE, "RequestedSecurityToken");
-        }
-        catch (Exception e)
-        {
-            throw new WSTrustException("Exception in renewing token:", e);
-        }
+      request.setTokenType(URI.create(WSTrustConstants.STATUS_TYPE));
+      request.setRequestType(URI.create(WSTrustConstants.RENEW_REQUEST));
+      RenewTargetType renewTarget = new RenewTargetType();
+      renewTarget.setAny(token);
+      request.setRenewTarget(renewTarget);
 
-        if (nodes == null)
-            throw new WSTrustException("NodeList is null");
+      // send the token request to JBoss STS and get the response.
+      WSTrustJAXBFactory jaxbFactory = WSTrustJAXBFactory.getInstance();
+      DOMSource requestSource = (DOMSource) jaxbFactory.marshallRequestSecurityToken(request);
+      Source response = dispatchLocal.get().invoke(requestSource);
 
-        Node rstr = nodes.item(0);
+      Node documentNode = ((DOMSource) response).getNode();
+      Document responseDoc = documentNode instanceof Document ? (Document) documentNode : documentNode
+            .getOwnerDocument();
 
-        return (Element) rstr.getFirstChild();
+      NodeList nodes;
+      try
+      {
+         Document myDocument = DocumentUtil.createDocument();
+         Node importedNode = myDocument.importNode(responseDoc.getDocumentElement(), true);
+         myDocument.appendChild(importedNode);
 
-    }
+         nodes = null;
+         if (responseDoc instanceof SOAPPart)
+         {
+            SOAPPart soapPart = (SOAPPart) responseDoc;
+            SOAPEnvelope env = soapPart.getEnvelope();
+            SOAPBody body = env.getBody();
+            Node data = body.getFirstChild();
+            nodes = ((Element) data).getElementsByTagNameNS(WSTrustConstants.BASE_NAMESPACE, "RequestedSecurityToken");
+            if (nodes == null || nodes.getLength() == 0)
+               nodes = ((Element) data).getElementsByTagName("RequestedSecurityToken");
+         }
+         else
+         {
+            nodes = responseDoc.getElementsByTagNameNS(WSTrustConstants.BASE_NAMESPACE, "RequestedSecurityToken");
+            if (nodes == null || nodes.getLength() == 0)
+               nodes = responseDoc.getElementsByTagName("RequestedSecurityToken");
+         }
+      }
+      catch (Exception e)
+      {
+         throw new WSTrustException("Exception in renewing token:", e);
+      }
 
-    public boolean validateToken(Element token) throws WSTrustException
-    {
-        RequestSecurityToken request = new RequestSecurityToken();
-        request.setContext("context");
+      if (nodes == null)
+         throw new WSTrustException("NodeList is null");
 
-        request.setTokenType(URI.create(WSTrustConstants.STATUS_TYPE));
-        request.setRequestType(URI.create(WSTrustConstants.VALIDATE_REQUEST));
-        ValidateTargetType validateTarget = new ValidateTargetType();
-        validateTarget.setAny(token);
-        request.setValidateTarget(validateTarget);
+      Node rstr = nodes.item(0);
 
-        WSTrustJAXBFactory jaxbFactory = WSTrustJAXBFactory.getInstance();
+      return (Element) rstr.getFirstChild();
 
-        DOMSource requestSource = (DOMSource) jaxbFactory.marshallRequestSecurityToken(request);
+   }
 
-        Source response = dispatchLocal.get().invoke(requestSource);
-        RequestSecurityTokenResponseCollection responseCollection = (RequestSecurityTokenResponseCollection) jaxbFactory.parseRequestSecurityTokenResponse(response);
-        RequestSecurityTokenResponse tokenResponse = responseCollection.getRequestSecurityTokenResponses().get(0);
+   public boolean validateToken(Element token) throws WSTrustException
+   {
+      RequestSecurityToken request = new RequestSecurityToken();
+      request.setContext("context");
 
-        StatusType status = tokenResponse.getStatus();
-        if (status != null)
-        {
-            String code = status.getCode();
-            return WSTrustConstants.STATUS_CODE_VALID.equals(code);
-        }
-        return false;
-    }
+      request.setTokenType(URI.create(WSTrustConstants.STATUS_TYPE));
+      request.setRequestType(URI.create(WSTrustConstants.VALIDATE_REQUEST));
+      ValidateTargetType validateTarget = new ValidateTargetType();
+      validateTarget.setAny(token);
+      request.setValidateTarget(validateTarget);
 
-    public Dispatch<Source> getDispatch()
-    {
-        return dispatchLocal.get();
-    }
+      WSTrustJAXBFactory jaxbFactory = WSTrustJAXBFactory.getInstance();
+
+      DOMSource requestSource = (DOMSource) jaxbFactory.marshallRequestSecurityToken(request);
+
+      Source response = dispatchLocal.get().invoke(requestSource);
+      RequestSecurityTokenResponseCollection responseCollection = (RequestSecurityTokenResponseCollection) jaxbFactory
+            .parseRequestSecurityTokenResponse(response);
+      RequestSecurityTokenResponse tokenResponse = responseCollection.getRequestSecurityTokenResponses().get(0);
+
+      StatusType status = tokenResponse.getStatus();
+      if (status != null)
+      {
+         String code = status.getCode();
+         return WSTrustConstants.STATUS_CODE_VALID.equals(code);
+      }
+      return false;
+   }
+
+   public Dispatch<Source> getDispatch()
+   {
+      return dispatchLocal.get();
+   }
 }
