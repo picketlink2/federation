9687b0cb6167b8384a081d37908819c63819fb52
Anil Saldhana <anil.saldhana@jboss.com>
2010-11-24 23:56:13 +0000

    the saml protocol classes
    git-svn-id: https://svn.jboss.org/repos/picketlink/federation/trunk@562 cbddccc4-e45a-42eb-be77-0d76e4b45d38

---
+++ picketlink-fed-model/src/main/java/org/picketlink/identity/federation/newmodel/saml/v2/protocol/ResponseType.java
@@ -0,0 +1,90 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors. 
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.picketlink.identity.federation.newmodel.saml.v2.protocol;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.picketlink.identity.federation.newmodel.saml.v2.assertion.EncryptedAssertionType;
+import org.picketlink.identity.federation.saml.v2.assertion.AssertionType;
+
+/**
+ * <p>Java class for ResponseType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="ResponseType">
+ *   &lt;complexContent>
+ *     &lt;extension base="{urn:oasis:names:tc:SAML:2.0:protocol}StatusResponseType">
+ *       &lt;choice maxOccurs="unbounded" minOccurs="0">
+ *         &lt;element ref="{urn:oasis:names:tc:SAML:2.0:assertion}Assertion"/>
+ *         &lt;element ref="{urn:oasis:names:tc:SAML:2.0:assertion}EncryptedAssertion"/>
+ *       &lt;/choice>
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */ 
+public class ResponseType
+extends StatusResponseType
+{ 
+   protected List<ChoiceType> assertions = new ArrayList<ResponseType.ChoiceType>();
+
+   public void addAssertion( ChoiceType choice )
+   {
+      assertions.add(choice);
+   }
+
+   /**
+    * Gets a read only list of assertions
+    */
+   public List<ChoiceType> getAssertions() 
+   {
+      return Collections.unmodifiableList( assertions );
+   }
+
+   public static class ChoiceType
+   {
+      private AssertionType assertion;
+      private EncryptedAssertionType encryptedAssertion;
+      public ChoiceType(AssertionType assertion)
+      { 
+         this.assertion = assertion;
+      }
+      public ChoiceType(EncryptedAssertionType encryptedAssertion)
+      { 
+         this.encryptedAssertion = encryptedAssertion;
+      }
+      public AssertionType getAssertion()
+      {
+         return assertion;
+      }
+      public EncryptedAssertionType getEncryptedAssertion()
+      {
+         return encryptedAssertion;
+      } 
+   } 
+}
