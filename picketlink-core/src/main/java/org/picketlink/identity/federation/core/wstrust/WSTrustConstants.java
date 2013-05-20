dc8f1d0e3df77cefee7d39f1b4581997dd101b8d
Anil Saldhana <Anil.Saldhana@jboss.com>
2013-05-20 14:44:30 -0500

    PLINK2-36: STS parsing RST should not choke on WS-Policy 1.5 tags

--- picketlink-core/src/main/java/org/picketlink/identity/federation/core/wstrust/WSTrustConstants.java
+++ picketlink-core/src/main/java/org/picketlink/identity/federation/core/wstrust/WSTrustConstants.java
@@ -77,6 +77,8 @@ public interface WSTrustConstants {
     String WSA_NS = "http://www.w3.org/2005/08/addressing";
 
     String WSP_NS = "http://schemas.xmlsoap.org/ws/2004/09/policy";
+    
+    String WSP_15_NS = "http://www.w3.org/ns/ws-policy";
 
     String WSU_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
 
