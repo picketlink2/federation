9e98b2434843ee689bc8e5949c1b6afc6257c37b
Derek Horton <dehort@redhat.com>
2014-04-04 10:57:29 -0500

    Backport PLINK-405 - Fixed an issue where the principal that gets sent to the mapping provider is wrong

--- picketlink-bindings/picketlink-tomcat-common/src/main/java/org/picketlink/identity/federation/bindings/tomcat/idp/AbstractIDPValve.java
+++ picketlink-bindings/picketlink-tomcat-common/src/main/java/org/picketlink/identity/federation/bindings/tomcat/idp/AbstractIDPValve.java
@@ -151,6 +151,8 @@ public abstract class AbstractIDPValve extends ValveBase {
     private Map<String, SPSSODescriptorType> spSSOMetadataMap = new HashMap<String, SPSSODescriptorType>();
     private SSLAuthenticator sslAuthenticator;
 
+    private Boolean passUserPrincipalToAttributeManager = false;
+
     // Set a list of attributes we are interested in separated by comma
     public void setAttributeList(String attribList) {
         if (StringUtil.isNotNull(attribList)) {
@@ -243,6 +245,15 @@ public abstract class AbstractIDPValve extends ValveBase {
     public void setSignOutgoingMessages(Boolean signOutgoingMessages) {
         logger.warn("Option signOutgoingMessages is used for signing of error messages. Normal SAML messages are "
                 + "signed by SAML2SignatureGenerationHandler.");
+    }
+
+    /**
+     * IDP should get the user principal from Request.getUserPrincipal() and send that to the attribute manager
+     *
+     * @param passUserPrincipalToAttributeManager
+     */
+    public void setPassUserPrincipalToAttributeManager(Boolean passUserPrincipalToAttributeManager) {
+        this.passUserPrincipalToAttributeManager = passUserPrincipalToAttributeManager;
     }
 
     /**
@@ -696,7 +707,11 @@ public abstract class AbstractIDPValve extends ValveBase {
                 List<String> roles = roleGenerator.generateRoles(userPrincipal);
                 session.getSession().setAttribute(GeneralConstants.ROLES_ID, roles);
 
-                Map<String, Object> attribs = this.attribManager.getAttributes(userPrincipal, attributeKeys);
+                Map<String, Object> attribs = this.attribManager.getAttributes(
+                                     passUserPrincipalToAttributeManager == true 
+                                         ?  request.getUserPrincipal() 
+                                         : userPrincipal, 
+                                     attributeKeys);
                 requestOptions.put(GeneralConstants.ATTRIBUTES, attribs);
             }
 
